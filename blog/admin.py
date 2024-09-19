f2eaaffd2e6d67e41b90c050c3a32a88807881bf
QuvonchbekCoder05 <166432175+QuvonchbekCoder05@users.noreply.github.com>
2024-09-19 10:04:32 +0500

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from .models import Blog, Comment
 
-# Blog modelini admin panelida ro'yxatdan o'tkazamiz
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_at')
     search_fields = ('title', 'author__username')
     list_filter = ('created_at',)
 
-# Comment modelini admin panelida ro'yxatdan o'tkazamiz
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('blog', 'user', 'created_at')
